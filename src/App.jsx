e3d280d367e66a499ed1cfda47269209df106d0e
Prashant Raut <prashantraut1308@gmail.com>
2025-06-10 14:34:24 +0530

    Chat Application baisc pages folder structur and assets added

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import react from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomepPage from './pages/HomepPage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+
+import bgImage from './assets/bgImage.svg'
+
+const App = () => {
+  return (
+    <div className="bg-contain, bg-no-repeat, h-screen w-full"  style={{ backgroundImage: `url(${bgImage})` }}
+    >
+
+      
+      <Routes>
+        <Route path='/' element={<HomepPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/HomepPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomepPage() {
+  return (
+    <div>HomepPage</div>
+  )
+}
+
+export default HomepPage
