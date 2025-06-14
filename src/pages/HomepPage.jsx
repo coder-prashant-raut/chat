8933b9c5df84ea6a77d70686789590998268cfec
Prashant Raut <prashantraut1308@gmail.com>
2025-06-14 11:19:57 +0530

    updates sidebar.jsx and added props to all component in homepage.jsx

--- src/pages/HomepPage.jsx
+++ src/pages/HomepPage.jsx
@@ -11,9 +11,9 @@ function HomepPage() {
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%] '>
       
       <div className={`backdrop-blur-xl border-r-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUSer ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'} `}>  
-      <Sidebar/>
-      <ChatContainer/>
-      <RightSidebar/>
+      <Sidebar selectedUser={selectedUSer} setSelectedUser={setSelectedUSer} />
+      <ChatContainer selectedUser={selectedUSer} setSelectedUser={setSelectedUSer}/>
+      <RightSidebar selectedUser={selectedUSer} setSelectedUser={setSelectedUSer}/>
  
       </div>
       
